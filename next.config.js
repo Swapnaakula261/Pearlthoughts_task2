c03e416c452d515a25281a0dfcdd1104fdf7a345
Gopi Krishna Adusumalli <gopi@Gopis-MacBook-Air.local>
2024-09-12 17:57:05 +0530

    Initial commit for gh-pages

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Pearlthoughts_task2', // Set this to your repository name
+  assetPrefix: '/Pearlthoughts_task2/',
+};
+
